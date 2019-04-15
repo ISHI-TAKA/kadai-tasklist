c89f54fc5bf12ca1bf29c449a8b9f7b79572894f
ISHI-TAKA <ishi-taka88888@gmail.com>
2019-04-15 13:04:21 +0000

    Submissio_20190415

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,9 +46,10 @@ class TasksController < ApplicationController
      flash[:success] = 'Taskは正常に削除されました'
      redirect_to tasks_url
    end
-   
-   
-   # Strong Parameter
+  
+  private
+
+  # Strong Parameter
    def task_params
      params.require(:task).permit(:content, :status)
    end
